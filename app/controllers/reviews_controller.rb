eb11a87803c7e895c40026baa13a9b748706d8ce
Mai Hoang <maihh97@gmail.com>
2021-04-24 16:13:57 +0100

    Refactor to get rid of GET route for Review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,9 +1,10 @@
 class ReviewsController < ApplicationController
   before_action :find_restaurant, except: [:destroy]
 
-  def new
-    @review = Review.new
-  end
+#doesn't need the new because created in the render form, go to restaurants/show.html.erb
+  # def new
+  #   @review = Review.new
+  # end
 
   def create
     @review = Review.new(review_params)
