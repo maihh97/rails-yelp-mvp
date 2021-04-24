9a62ea19db5c6d45d210d228ac7c992c25655d8f
Mai Hoang <maihh97@gmail.com>
2021-04-24 15:05:32 +0100

    Yelp MVP before getting rid of GET route in Review

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :restaurants, only: [:new, :create, :index, :show] do
-    resources :reviews, only: [:create]
+    resources :reviews, only: [:create, :new]
   end
 end
